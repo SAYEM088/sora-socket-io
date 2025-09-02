e984ccdc7696c7794c0cc8601b4fffb394e6f801
SAYEM088 <abusayemsarker088@gmail.com>
2025-09-02 23:34:17 +0600

    frontend added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: "https://sora-test-kappa.vercel.app/" || "http://localhost:3000",
     methods: ["GET", "POST"],
   },
   pingTimeout: 20000,
